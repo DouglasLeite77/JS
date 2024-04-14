ba7bd9ec343ffb172cd3cbaa9e268376c8d2e8f6
DouglasLeite77 <douglasleitee255@gmail.com>
2024-04-14 13:08:26 -0300

    limpeza

--- aulas facul/tema8/temperatura/tema8.3.js
+++
@@ -1,9 +0,0 @@
-function converter(){
-    var temp1 = window.document.getElementById("temperatura1")
-    var resultado = window.document.getElementById("resultado")
-    var Temp1 = Number(temp1.value)
-
-    Resultado = (Temp1 - 32) * 5 / 9
-
-    resultado.value = Temp1 + " ºF = " + Resultado.toFixed(2) + " ºC" 
-}
--- exercícios/ex01/ex01.js
+++
@@ -1,9 +0,0 @@
-function calcular(){
-    
-    var n1 = Number(document.getElementById('n1').value)
-    var n2 = Number(document.getElementById('n2').value)
-    var res = document.getElementById('res')
-
-    var soma = n1 + n2
-    res.value = soma
-}
