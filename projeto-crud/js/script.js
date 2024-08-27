a8ce45d6da431e98c009061db80343a039edfebf
DouglasLeite77 <douglasleitee255@gmail.com>
2024-08-26 22:06:37 -0300

    put parte 01

--- projeto-crud/js/script.js
+++ projeto-crud/js/script.js
@@ -34,12 +34,48 @@ formulario.addEventListener("submit", enviarDados)
 
 async function enviarDados(event){
     event.preventDefault()
+    const buscarApi = await fetch(api)
+    const pensamentos = await buscarApi.json()
 
     const conteudo = document.querySelector("#pensamento-conteudo").value
     const id = document.querySelector("#pensamento-id").value
     const autoria = document.querySelector("#pensamento-autoria").value
 
-    await salvarPensamento({conteudo, autoria})
+    await salvarPensamento({ conteudo, autoria});
     mostrarPensamentos()
+}
 
- }
+
+async function buscaPensamento(id) {
+    const buscarpensamento = await fetch(`http://localhost:3000/pensamentos/${id}`)
+    const pensamentos = await buscarpensamento.json()
+
+    let conteudo = document.querySelector("#pensamento-conteudo")
+    let autoria = document.querySelector("#pensamento-autoria")
+    let idPensamento = document.querySelector("#pensamento-id")
+    idPensamento.value = id
+    autoria.value = pensamentos.autoria
+    conteudo.value = pensamentos.conteudo
+}
+buscaPensamento("9bf2")
+
+async function editarPensamento(pensamento) {
+    const buscarApi = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+    })
+}
+
+const btnCacenlar = document.querySelector("#botao-cancelar")
+
+btnCacenlar.addEventListener('click', () => {
+    let conteudo = document.querySelector("#pensamento-conteudo")
+    let autoria = document.querySelector("#pensamento-autoria")
+    autoria.value = ''
+    conteudo.value = ''
+})
+
+
